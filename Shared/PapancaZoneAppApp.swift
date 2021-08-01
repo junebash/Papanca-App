5f5dc97c5cfdb26519ed436de43e277425ccd744
June Bash <junebash@pm.me>
2021-08-01 09:50:44 -0700

    Initial Commit

---
+++ Shared/PapancaZoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapancaZoneAppApp.swift
+//  Shared
+//
+//  Created by Jon Bash on 2021-08-01.
+//
+
+import SwiftUI
+
+@main
+struct PapancaZoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
